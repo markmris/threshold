34b2be34cc741b63fde8b5f4ea9a577757b035c6
markmris <mark.mrishih@gmail.com>
2026-01-31 11:22:36 -0400

    Added Generator Stat/Health controls.

--- Assets/Scripts/GeneratorController.cs
+++ Assets/Scripts/GeneratorController.cs
@@ -18,11 +18,66 @@ public class GeneratorController : MonoBehaviour
     public Color defaultBackgroundColor;
     public Color interactBackgroundColor;
 
+    private float stability = 40f;
+    private float tempurature = 15f;
+    private float psi = 10f;
+    private bool switchActivated = false;
+    private bool powerOn = true;
+
+    static private float baseTempSpeed = 0.4f;
+    static private float basePsiSpeed = 0.15f;
+    static private float stabilitySpeed = 0.2f;
+    private float tempSpeed = baseTempSpeed;
+    private float psiSpeed = basePsiSpeed;
+
     private Camera cam;
 
     void Start()
     {
         cam = Camera.main;
+    }
+
+    void Update()
+    {
+        tempSpeed = baseTempSpeed;
+        psiSpeed = basePsiSpeed;
+        stabilitySpeed = 0.2f;
+
+        if (!powerOn)
+        {
+            tempSpeed = -0.4f;
+            psiSpeed  = -0.3f;
+            stabilitySpeed = -0.2f;
+        }
+        else if (switchActivated)
+        {
+            tempSpeed *= 0.5f;
+            psiSpeed  *= 1.8f;
+            stabilitySpeed += 0.3f;
+        }
+
+        if (stability < 35f)
+        {
+            tempSpeed += 0.5f;
+            psiSpeed += 0.7f;
+        }
+
+        if (tempurature > 50f)
+        {
+            stabilitySpeed += 0.2f;
+        }
+        if (psi > 80f)
+        {
+            stabilitySpeed += 0.4f;
+        }
+
+        tempurature += tempSpeed * Time.deltaTime;
+        psi += psiSpeed * Time.deltaTime;
+        stability -= stabilitySpeed * Time.deltaTime;
+
+        tempurature = Mathf.Clamp(tempurature, 0f, 100f);
+        psi = Mathf.Clamp(psi, 0f, 100f);
+        stability = Mathf.Clamp(stability, 0f, 100f);
     }
 
     public void HandleClick(GameObject clickedObject)
@@ -31,6 +86,17 @@ public class GeneratorController : MonoBehaviour
         {
             cam.transform.position = defaultCamPosition;
             cam.backgroundColor = defaultBackgroundColor;
+        }
+
+        else if (clickedObject == shutdownButtonClickable)
+        {
+            powerOn = !powerOn;
+            stability -= 3f;
+
+            switchActivated = false;
+            Quaternion rotation = powerSwitchVFX.transform.rotation;
+            rotation.z = 50f;
+            powerSwitchVFX.transform.rotation = rotation;
         }
 
         else if (clickedObject == pressureMeterClickable)
@@ -50,6 +116,7 @@ public class GeneratorController : MonoBehaviour
             Quaternion rotation = powerSwitchVFX.transform.rotation;
             rotation.z *= -1;
             powerSwitchVFX.transform.rotation = rotation;
+            switchActivated = !switchActivated;
         }
     }
 }
